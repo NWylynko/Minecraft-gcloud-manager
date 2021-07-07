6c8d4b3c342620f03b07e2421e858b6201f5e637
Nick Wylynko <nick1014375@gmail.com>
2021-07-07 22:46:37 +0800

    bloody cors

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,35 @@
 const functions = require("firebase-functions");
 const Compute = require('@google-cloud/compute');
+const cors = require('cors')
+
+const corsHandler = cors({ origin: "https://mc-control.wylynko.com" })
 
 const compute = new Compute();
 const zone = compute.zone('australia-southeast2-a');
 const mcServerVm = zone.vm('mc-server-1')
 
 const start = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Starting server", {structuredData: true});
-  try {
-    const data = await mcServerVm.start()
-    response.json(data);
-  } catch (error) {
-    response.json({ error });
-  }
+  corsHandler(request, response, async () => {
+    functions.logger.info("Starting server", { structuredData: true });
+    try {
+      const data = await mcServerVm.start()
+      response.json(data);
+    } catch (error) {
+      response.json({ error });
+    }
+  })
 });
 
 const stop = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Stopping server", {structuredData: true});
-  try {
-    const data = await mcServerVm.stop()
-    response.json(data);
-  } catch (error) {
-    response.json({ error });
-  }
+  corsHandler(request, response, async () => {
+    functions.logger.info("Stopping server", { structuredData: true });
+    try {
+      const data = await mcServerVm.stop()
+      response.json(data);
+    } catch (error) {
+      response.json({ error });
+    }
+  })
 });
 
 module.exports = {
